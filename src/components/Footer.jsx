f6032b9d05ec766e1373b39e3b140aa512d94417
Mayank Rawat <mayankrawat55716@gmail.com>
2024-03-07 21:37:26 +0530

    deploy

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>5 STAR</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://instagram.com/mayank_pahadi_12/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/m1-rawat" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
